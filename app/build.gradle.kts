41edf88efecd7eb697b18dd98d644df58d74c75e
Benjamin <b.valenzuela06@ufromail.cl>
2025-05-17 14:44:29 -0400

    fix: add missing Firebase Auth and Firestore dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,4 +38,6 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
     implementation(platform("com.google.firebase:firebase-bom:33.13.0"))
+    implementation("com.google.firebase:firebase-firestore")
+    implementation("com.google.firebase:firebase-auth")
 }
