2355ec955f124aef5962e6eeb8e4eb615f3efc74
Benjamin <b.valenzuela06@ufromail.cl>
2025-05-17 21:38:34 -0400

    Implementing AuthManager

---
+++ app/src/main/java/com/example/dozy/auth/AuthManager.java
@@ -0,0 +1,26 @@
+package com.example.dozy.auth;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class AuthManager {
+    private FirebaseAuth auth = FirebaseAuth.getInstance();
+
+    public Task<AuthResult> login(String email, String password){
+        return auth.signInWithEmailAndPassword(email,password);
+    }
+
+    public Task<AuthResult> register(String email,String password){
+        return auth.createUserWithEmailAndPassword(email,password);
+    }
+
+    public FirebaseUser getCurrentUser(){
+        return auth.getCurrentUser();
+    }
+
+    public void logout(){
+        auth.signOut();
+    }
+}
