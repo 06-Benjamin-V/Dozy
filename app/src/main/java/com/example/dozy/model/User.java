ddc0b28a7262ff37e01093eb7bed69cfff6538e5
Benjamin <b.valenzuela06@ufromail.cl>
2025-06-27 00:12:43 -0400

    update of the User model

--- app/src/main/java/com/example/dozy/model/User.java
+++ app/src/main/java/com/example/dozy/model/User.java
@@ -1,15 +1,28 @@
 package com.example.dozy.model;
 
 public class User {
+    private String id;
     private String username;
-    private String phone;
     private String email;
+    private String phone;
+    private String password;
 
-    public User() {}
-    public User(String username, String phone, String email) {
+    public User() {
+    }
+    public User(String id, String username, String email, String phone, String password) {
+        this.id = id;
         this.username = username;
+        this.phone = email;
         this.phone = phone;
-        this.email = email;
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getUsername() {
@@ -34,5 +47,13 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
